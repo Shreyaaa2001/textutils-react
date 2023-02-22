69eeced78a5f40c6737d62de2b36fb1ac2349a4d
Shreya <shreyanadiger@gmail.com>
2023-02-22 10:43:08 +0530

    Updates

---
+++ precache-manifest.133cd98e4e7332e49c42e1aa8d9f8c4b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "508754077e1d33f568bc",
+    "url": "/textutils-react/static/css/main.612c3b6d.chunk.css"
+  },
+  {
+    "revision": "508754077e1d33f568bc",
+    "url": "/textutils-react/static/js/main.50875407.chunk.js"
+  },
+  {
+    "revision": "3c11774c00c8fe3168dd",
+    "url": "/textutils-react/static/js/1.3c11774c.chunk.js"
+  },
+  {
+    "revision": "2b8e73089cc5c607c393",
+    "url": "/textutils-react/static/js/2.2b8e7308.chunk.js"
+  },
+  {
+    "revision": "3e897f2210a78e65956d",
+    "url": "/textutils-react/static/js/runtime~main.3e897f22.js"
+  },
+  {
+    "revision": "d7f8c65803e642406305f725b9769a2d",
+    "url": "/textutils-react/index.html"
+  }
+];
